faa3e5a2c1791f1450c636e987ef4d290b795e5c
tanmay-deev <tanmaybonde10@gmail.com>
2026-06-12 18:06:54 +0530

    added Cors and Frontend Url

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import testRoutes from "./routes/testRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+
 import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -15,23 +18,30 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/products", productRoutes);
 
+// Root Route
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
 // Global Error Handler
 app.use(errorHandler);
-
-// Test Route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
